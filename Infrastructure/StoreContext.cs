422318c756753cb3bbbe4aedca4ce7ad6fcf755f
TobiasParnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-07 13:13:41 +0200

    StoreContext

---
+++ Infrastructure/StoreContext.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure
+{
+    public class StoreContext : DbContext
+    {
+       public StoreContext(DbContextOptions options) : base(options)
+       {
+       }
+
+       public DbSet<Course> Courses {get; set;}
+    }
+}
