fc22d7a2988fcbe9b9b4dbadd73612b6f9c6347d
TobiasParnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-12 15:27:39 +0200

    Stuck

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,19 @@
-import React, {useState, useEffect}from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
+
+import{Route, Switch} from "react-router-dom";
 import './App.css';
+import Homepage from './pages/Homepage';
+import LoginPage from './pages/Login';
+import DetailPage from './pages/DetailPage';
 
 function App() {
-  const [courses, setCourses] = useState([]);
-
-useEffect(() => {
- axios.get("http://localhost:5000/api/courses").then((response) => {
-   console.log(response)  
-   setCourses(response.data)
-});
-}, []);
-
   return (
-    <div className="App">
-      <ul>
-      {courses.map((course: any, index) => {
-        return(
-          <li key={index}>
-            {course.id}
-            {course.title}
-          </li>
-        )
-      })}
-      </ul>
-    </div>
+  <> 
+  <Switch> 
+  <Route exact path="/" component={Homepage} />
+  <Route exact path="/login" component={LoginPage} />
+  <Route exact path="/detail" component={DetailPage} />
+  </Switch>
+  </>
   );
 }
 
