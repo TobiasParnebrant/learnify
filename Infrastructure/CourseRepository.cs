cdf47350b25a4cff992660768b303d17e4c508a3
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-14 16:26:14 +0200

    Module3Start

---
+++ Infrastructure/CourseRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entity;
+using Entity.Interfaces;
+
+
+namespace Infrastructure
+{
+    public class CourseRepository: ICourseRepository
+    {
+
+        public Task<Course> GetCourseByIdAsync(int id)
+        {
+            throw new System.NotImplementedExeption();
+        }
+    }
+}
