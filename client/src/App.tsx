5a00e2a07618020aa23450efd4954b20b578f69e
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-21 15:34:57 +0200

    Refactoring and Adding Basket Items Count

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/Login";
@@ -9,8 +9,31 @@ import Categories from "./Components/Categories";
 import CategoryPage from "./pages/CategoryPage";
 import DescriptionPage from "./pages/DescriptionPage";
 import BasketPage from "./pages/BasketPage";
+import { useStoreContext } from "./context/StoreContext";
+import agent from "./actions/agent";
 
 function App() {
+	const { setBasket } = useStoreContext();
+
+	function getCookie(name: string) {
+		return (
+			document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() ||
+			""
+		);
+	}
+
+	useEffect(() => {
+		const clientId = getCookie("clientId");
+
+		if (clientId) {
+			agent.Baskets.get()
+				.then((response) => {
+					setBasket(response);
+				})
+				.catch((error) => console.log(error));
+		}
+	}, [setBasket]);
+
 	return (
 		<>
 			<Navigation />
@@ -18,7 +41,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route exact path="/category/:id" component={CategoryPage} />
-        <Route exact path="/basket" component={BasketPage} />
+				<Route exact path="/basket" component={BasketPage} />
 				<Route exact path="/course/:id" component={DescriptionPage} />
 				<Route exact path="/login" component={LoginPage} />
 				<Route exact path="/detail" component={DetailPage} />
--- client/src/pages/BasketPage.tsx
+++ client/src/pages/BasketPage.tsx
@@ -1,74 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import { Table } from 'antd'
-import agent from '../actions/agent'
-import * as FaIcons from 'react-icons/fa'
-import { Basket, CourseItem } from '../models/basket'
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
+import agent from "../actions/agent";
+import * as FaIcons from "react-icons/fa";
+import { Basket, CourseItem } from "../models/basket";
+import { useStoreContext } from "../context/StoreContext";
 
 const BasketPage = () => {
-  const [items, setItems] = useState<Basket | null>()
+  const [items, setItems] = useState<Basket | null>();
+
+  const { basket, removeItem } = useStoreContext();
+
+  const basketCount = basket?.items.length;
 
   const removeBasketItem = (courseId: string) => {
-    agent.Baskets.removeItem(courseId).catch((error) => {
-      console.log(error)
-    })
-  }
+    agent.Baskets.removeItem(courseId)
+      .then(() => removeItem(courseId))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const newData = (items: Basket | null) => {
     items?.items.map((item: CourseItem, index: number) =>
-      Object.assign(item, { key: index }),
-    )
-    setItems(items)
-  }
+      Object.assign(item, { key: index })
+    );
+    setItems(items);
+  };
 
   useEffect(() => {
-    agent.Baskets.get().then((response) => {
-      newData(response)
-    })
-  }, [])
+    newData(basket);
+  }, [basket]);
 
   const columns = [
     {
-      title: 'Image',
-      dataIndex: 'image',
-      key: 'image',
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
       render: (url: string) => {
-        return <img width="100px" src={url} alt={url} />
+        return <img width="100px" src={url} alt={url} />;
       },
     },
     {
-      title: 'Title',
-      dataIndex: 'title',
-      key: 'title',
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
     },
     {
-      title: 'Price',
-      dataIndex: 'price',
-      key: 'price',
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
     },
     {
-      title: 'Instructor',
-      key: 'instructor',
-      dataIndex: 'instructor',
+      title: "Instructor",
+      key: "instructor",
+      dataIndex: "instructor",
     },
     {
-      title: 'Action',
-      key: 'action',
+      title: "Action",
+      key: "action",
       render: (item: CourseItem) => (
         <div onClick={() => removeBasketItem(item.courseId)}>
           <FaIcons.FaTrash />
         </div>
       ),
     },
-  ]
+  ];
 
   return (
     <>
       <div className="basket-page">
         <h1 className="basket-page__header">Shopping Cart</h1>
+        <h2 className="basket-page__sub-header">
+          {`${basketCount} ${
+            basketCount! > 1 ? "courses" : "course"
+          } in the Cart`}
+        </h2>
         <Table columns={columns} dataSource={items?.items} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default BasketPage
+export default BasketPage;
