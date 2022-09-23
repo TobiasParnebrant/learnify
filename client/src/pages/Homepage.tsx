93eb36d88f54e8bc9c965aad6f9326b9487a4315
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-23 09:38:31 +0200

    Adding Frontend Pagination

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,64 +1,83 @@
-import React, { useEffect } from "react";
-import { Card, Col, Radio, Row } from "antd";
-import { Course } from "../models/course";
-import ShowCourses from "../Components/ShowCourses";
-import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
-import { courseSelector, getCoursesAsync } from "../redux/slice/courseSlice";
-import { Category } from "../models/category";
-import { categoriesSelector } from "../redux/slice/categorySlice";
+import React, { useEffect } from 'react';
+import { Card, Col, Radio, Row } from 'antd';
+import { Course } from '../models/course';
+import ShowCourses from '../Components/ShowCourses';
+import { useAppDispatch, useAppSelector } from '../redux/store/configureStore';
+import {
+  coursesSelector,
+  getCoursesAsync,
+  setCourseParams,
+} from '../redux/slice/courseSlice';
+import { categoriesSelector } from '../redux/slice/categorySlice';
+import { Category } from '../models/category';
 
 const sortOptions = [
-	{ value: "title", label: "Alphabetical" },
-	{ value: "priceDescending", label: "Price - High to low" },
-	{ value: "priceAscending", label: "Price - Low to high" },
+  { value: 'title', label: 'Alphabetical' },
+  { value: 'priceDescending', label: 'Price - High to low' },
+  { value: 'priceAscending', label: 'Price - Low to high' },
 ];
 
 const Homepage = () => {
-	const courses = useAppSelector(courseSelector.selectAll);
-	const dispatch = useAppDispatch();
-	const { coursesLoaded } = useAppSelector((state) => state.course);
+  const data = useAppSelector(coursesSelector.selectAll);
+  const dispatch = useAppDispatch();
+  const { coursesLoaded, courseParams } = useAppSelector(
+    (state) => state.course,
+  );
+  const categories = useAppSelector(categoriesSelector.selectAll);
 
-	useEffect(() => {
-		if (!coursesLoaded) dispatch(getCoursesAsync());
-	}, [coursesLoaded, dispatch]);
+  const getCategories = () => {
+    const catArray: any[] = [];
+    categories.forEach((category: Category) => {
+      catArray.push({ value: category.id, label: category.name });
+    });
+    return catArray;
+  };
 
-	const categories = useAppSelector(categoriesSelector.selectAll);
+  useEffect(() => {
+    if (!coursesLoaded) dispatch(getCoursesAsync());
+  }, [coursesLoaded, dispatch]);
 
-	const getCategories = () => {
-		const catArray: any[] = [];
-		categories.forEach((category: Category) => {
-			catArray.push({ value: category.id, label: category.name });
-		});
-		return catArray;
-	};
-
-	return (
-		<div className="course">
-			<div className="course__header">
-				<h1>What to learn Next?</h1>
-				<h2>New Courses picked just for you...</h2>
-			</div>
-
-			<Row gutter={[24, 32]}>
-				<Col span={4}>
-					<Card title="Sorting Options">
-						<Radio.Group options={sortOptions} />
-					</Card>
-					<Card title="choose Category">
-						<Radio.Group options={getCategories()} />
-					</Card>
-				</Col>
-				<Col span={20}>
-					<Row gutter={[24, 32]}>
-						{courses &&
-							courses.map((course: Course, index: number) => {
-								return <ShowCourses key={index} course={course} />;
-							})}
-					</Row>
-				</Col>
-			</Row>
-		</div>
-	);
-};
-
-export default Homepage;
+  return (
+    <div className="course">
+      <div className="course__header">
+        <h1>What to learn Next?</h1>
+        <h2>New Courses picked just for you...</h2>
+      </div>
+      <Row gutter={[24, 32]}>
+        <Col span={4}>
+          <Card title="Sorting Options">
+            <Radio.Group
+              options={sortOptions}
+              value={courseParams.sort}
+              onChange={(e) =>
+                dispatch(setCourseParams({ sort: e.target.value }))
+              }
+            />
+          </Card>
+          <Card title="Choose Category">
+            <Radio.Group
+              options={getCategories()}
+              value={courseParams.category}
+              onChange={(e) => {
+                dispatch(setCourseParams({ category: e.target.value }));
+              }}
+            />
+          </Card>
+        </Col>
+        <Col span={20}>
+          <Row gutter={[24, 32]}>
+            {data &&
+              data.map((course: Course, index: number) => {
+                return (
+                  <ShowCourses
+                    key={index}
+                    course={course}
+                     />
+                );
+              })}
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+}
