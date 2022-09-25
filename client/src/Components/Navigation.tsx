70e3b51268fe2907e77355fc28610a78e6df1447
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-25 14:23:13 +0200

    Creating Login Dropdown

--- client/src/Components/Navigation.tsx
+++ client/src/Components/Navigation.tsx
@@ -1,86 +1,111 @@
 import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import { setCourseParams } from "../redux/slice/courseSlice";
+import { signOut } from "../redux/slice/userSlice";
 import { useAppSelector } from "../redux/store/configureStore";
+import UserMenu from "./UserMenu";
 
 const Navigation = () => {
-  const [sidebar, setSidebar] = useState(false);
-  const [searchText, setSearchText] = useState("");
-  const { basket } = useAppSelector((state) => state.basket);
-  const basketCount = basket?.items.length;
-  const showSidebar = () => setSidebar(!sidebar);
-  const dispatch = useDispatch();
+	const [sidebar, setSidebar] = useState(false);
+	const [searchText, setSearchText] = useState("");
+	const { basket } = useAppSelector((state) => state.basket);
+	const showSidebar = () => setSidebar(!sidebar);
+	const { user } = useAppSelector((state) => state.user);
+	const basketCount = basket?.items.length;
+	const dispatch = useDispatch();
+  const history = useHistory();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
+  const signout = () => {
+    dispatch(signOut());
+    history.push("/");
   };
 
-  const onSearch = (e: SyntheticEvent) => {
-    e.preventDefault();
-    dispatch(setCourseParams({ search: searchText }));
-  };
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchText(e.target.value);
+	};
 
-  return (
-    <div className="nav-container">
-      <div className="nav">
-        <div className="nav__left">
-          <div className="nav__left__hamburger">
-            <FaIcons.FaBars onClick={showSidebar} />
-            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-              <ul className="nav-menu-items" onClick={showSidebar}>
-                <li className="cancel">
-                  <FaIcons.FaChevronLeft />
-                </li>
+	const onSearch = (e: SyntheticEvent) => {
+		e.preventDefault();
+		dispatch(setCourseParams({ search: searchText }));
+	};
 
-                <li className="nav-menu-items__header">Navigation</li>
-                <Link to="/">
-                  <li>Home</li>
-                </Link>
-                <Link to="/login">
-                  <li>Login</li>
-                </Link>
-              </ul>
-            </nav>
-          </div>
-          <img src={Logo} className="nav__left__logo" alt="logo" />
+	return (
+		<div className="nav-container">
+			<div className="nav">
+				<div className="nav__left">
+					<div className="nav__left__hamburger">
+						<FaIcons.FaBars onClick={showSidebar} />
+						<nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+							<ul className="nav-menu-items" onClick={showSidebar}>
+								<li className="cancel">
+									<FaIcons.FaChevronLeft />
+								</li>
 
-          <ul className="nav__left__list">
-            <Link to="/">
-              <li className="nav__left__list__item">Home</li>
-            </Link>
-            <Link to="/login">
-              <li className="nav__left__list__item">Login</li>
-            </Link>
-          </ul>
-        </div>
-        <div className="nav__right">
-          <form onSubmit={onSearch} className="nav__right__search">
-            <input
-              type="text"
-              className="nav__right__search__input"
-              placeholder="Search Courses..."
-              value={searchText}
-              onChange={handleChange}
-            />
-            <button className="nav__right__search__button">
-              <i className="fas fa-search"></i>
-            </button>
-          </form>
-          <Link to="/basket">
-            <div className="nav__right__cart">
-              <FaIcons.FaShoppingCart />
-              {basketCount! > 0 && (
-                <span className="nav__right__cart__count">{basketCount}</span>
-              )}
-            </div>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+								<li className="nav-menu-items__header">Navigation</li>
+								<Link to="/">
+									<li>Home</li>
+								</Link>
+								{user ? (
+									<>
+										{" "}
+										<Link to="/profile"> <li>Profile</li></Link>{" "}
+										<div onClick={signout}>
+											<li>Logout</li>
+										</div>{" "}
+									</>
+								) : (
+									<Link to="/login">
+										<li>Login</li>
+									</Link>
+								)}
+							</ul>
+						</nav>
+					</div>
+					<img src={Logo} className="nav__left__logo" alt="logo" />
+
+					<ul className="nav__left__list">
+						<Link to="/">
+							<li className="nav__left__list__item">Home</li>
+						</Link>
+						{user ? (
+							<li className="nav__left__list__item">
+								<UserMenu />
+							</li>
+						) : (
+							<Link to="/login">
+								<li className="nav__left__list__item">Login</li>
+							</Link>
+						)}
+					</ul>
+				</div>
+				<div className="nav__right">
+					<form onSubmit={onSearch} className="nav__right__search">
+						<input
+							type="text"
+							className="nav__right__search__input"
+							placeholder="Search Courses..."
+							value={searchText}
+							onChange={handleChange}
+						/>
+						<button className="nav__right__search__button">
+							<i className="fas fa-search"></i>
+						</button>
+					</form>
+					<Link to="/basket">
+						<div className="nav__right__cart">
+							<FaIcons.FaShoppingCart />
+							{basketCount! > 0 && (
+								<span className="nav__right__cart__count">{basketCount}</span>
+							)}
+						</div>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navigation;
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,6 @@
+const Dashboard = () => {
+    return <h1>Dashboard</h1>;
+  };
+  
+  export default Dashboard;
+//Let's register it inside App.tsx. We can copy one from top and replace path with profile and component with Dashboard which will be imported automatically.
