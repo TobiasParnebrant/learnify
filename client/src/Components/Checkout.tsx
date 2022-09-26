82e44eab76fb38df513cb84e60a7a40f413e7e10
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-09-26 15:38:01 +0200

    Making Clear Basket Endpoint

--- client/src/Components/Checkout.tsx
+++ client/src/Components/Checkout.tsx
@@ -2,19 +2,67 @@ import {
     CardCvcElement,
     CardExpiryElement,
     CardNumberElement,
+    useElements,
+    useStripe,
   } from "@stripe/react-stripe-js";
-  import { Card, Form, Input } from "antd";
+  import { Card, Form, Input, notification } from "antd";
+import { SyntheticEvent } from "react";
   import { ChangeEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
+import agent from "../actions/agent";
+import { removeBasket } from "../redux/slice/basketSlice";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
   import CheckoutSummary from "./CheckoutSummary";
   
   const Checkout = () => {
     const [cardName, setCardName] = useState<string>("");
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const {basket} = useAppSelector((state) => state.basket);
+    const dispatch = useAppDispatch();
+    const history = useHistory();
   
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
       setCardName(e.target.value);
     };
   
     const [form] = Form.useForm();
+
+    const handlePayment = async (event: SyntheticEvent) => {
+      event.preventDefault();
+      if(!stripe || !elements) return;
+
+      try {
+        const cardElement = elements.getElement(CardNumberElement);
+        const paymentResult = await stripe.confirmCardPayment(basket?.clientSecret!, {
+          payment_method: {
+            card: cardElement!,
+            billing_details: {
+              name: cardName
+            }
+          }
+        })
+
+        if(paymentResult.paymentIntent?.status === "succeeded") {
+          notification.success({
+            message: "Your payment is Successful!"
+          });
+          dispatch(removeBasket());
+          await agent.Baskets.clear();
+          setTimeout(() => {
+            history.push("/profile");
+          }, 1000)
+        } else {
+          notification.error({
+            message: paymentResult.error?.message!,
+          })
+        }
+      } catch (error: any) {
+        console.log(error);
+      }
+    }
   
     return (
       <div className="checkout">
